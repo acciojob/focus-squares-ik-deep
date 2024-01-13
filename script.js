9c2a01e7b9a8075f90cd713e32b81bfde44e4d65
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-01-13 10:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+ function changeColors(event) {
+    var clickedBox = event.target;
+    var allBoxes = document.querySelectorAll('.square');
+
+    // Reset the background color of all boxes
+    allBoxes.forEach(function(box) {
+      box.style.backgroundColor = '';
+    });
+
+    // Change the background color of the clicked box
+    // clickedBox.style.backgroundColor = 'lightblue';
+
+    // Change the background color of the other boxes
+    allBoxes.forEach(function(box) {
+      if (box !== clickedBox) {
+        box.style.backgroundColor = '#6F4E37';
+      }
+    });
+  }
+
+  function resetColors() {
+    var allBoxes = document.querySelectorAll('.square');
+
+    // Reset the background color of all boxes
+    allBoxes.forEach(function(box) {
+      box.style.backgroundColor = '';
+    });
+  }
+
+  // Add event listeners to each box
+  var boxes = document.querySelectorAll('.square');
+  boxes.forEach(function(box) {
+    box.addEventListener('mouseover', changeColors);
+    box.addEventListener('mouseout', resetColors);
+  });
